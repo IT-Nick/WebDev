0f9e1e473bf225dc95196d3d25f17eb8a777c7b0
Stepan Plehanov <crayon-d@mail.ru>
2023-08-09 13:44:57 +0300

    add styles

--- frontends/mini-landings/app/(root)/page.tsx
+++ frontends/mini-landings/app/(root)/page.tsx
@@ -1,21 +1,8 @@
 "use client";
-import Layout from "@/components/components/layout";
-import Features from "@/components/components/layout/Features";
-import HeroHome from "@/components/components/partials/HeroHome";
-import Footer from "@/components/components/layout/Footer";
-
-import Stats from "@/components/components/layout/Stats";
 
 export default function Home() {
   return (
-    <Layout>
       <div className="flex flex-col md:flex-row">
-        <HeroHome />
       </div>
-      <Stats />
-      <Features />
-      {/* <Testimotals /> */}
-      <Footer />
-    </Layout>
   );
 }
