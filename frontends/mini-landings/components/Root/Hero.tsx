d7c648be281d6955c256b18d4380e1d97413c1e2
Stepan Plehanov <crayon-d@mail.ru>
2023-09-05 20:28:20 +0300

    new client

--- frontends/mini-landings/components/Root/Hero.tsx
+++ frontends/mini-landings/components/Root/Hero.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Hero.css';
 import Image from 'next/image';
+import { useRouter } from 'next/router'; // новый импорт
 
 interface HeroProps {
   loading: boolean;
@@ -9,39 +10,46 @@ interface HeroProps {
 export default function Hero({ loading }: HeroProps) {
   const [titleAnimation, setTitleAnimation] = useState(false);
   const [subTitleAnimation, setSubTitleAnimation] = useState(false);
+  const router = useRouter(); // новая строка
 
   useEffect(() => {
     if (!loading) {
       setTitleAnimation(true);
       const timer = setTimeout(() => {
         setSubTitleAnimation(true);
-      }, 2000); // 2 second delay to match the first title's animation duration
+      }, 2000);
 
       return () => clearTimeout(timer);
     }
   }, [loading]);
 
-  return (
-<div className="main-container-hero">
-  <div className="container-hero container-text-button-hero">
-    <div className="hero-title-container">
-      <h1 className={`hero-title ${!loading && titleAnimation ? 'animate-hero-title' : 'initial-offscreen'}`}>
-        Создавай будущее
-      </h1>
-    </div>
-    <div className="sub-title-container">
-      <h1 className={`sub-title-gradient ${!loading ? 'animate-sub-title-gradient' : ''}`}>вместе с нами!</h1>
-    </div>
-    <div className="sub-title">
-      <button className={`px-20 py-6 text-4xl font-bold text-rose-400 border border-rose-600 hover:bg-rose-600 hover:text-white focus:outline-none focus:ring active:bg-rose-500 ${!loading && subTitleAnimation ? 'animate-sub-title' : 'initial-hidden'}`}>
-        Присоединиться
-      </button>
-    </div>
-  </div>
-  <div className="container-hero container-image-hero">
-    <Image src="/heroimg.svg" alt="Next Image" width={600} height={600} className={`svg-icon ${!loading && subTitleAnimation ? 'animate-svg-icon' : 'initial-offscreen-right'}`} />
-  </div>
-</div>
+  const handleJoinClick = () => { // новая функция
+    router.push('/new');
+  };
 
+  return (
+    <div className="main-container-hero">
+      <div className="container-hero container-text-button-hero">
+        <div className="hero-title-container">
+          <h1 className={`hero-title ${!loading && titleAnimation ? 'animate-hero-title' : 'initial-offscreen'}`}>
+            Создавай будущее
+          </h1>
+        </div>
+        <div className="sub-title-container">
+          <h1 className={`sub-title-gradient ${!loading ? 'animate-sub-title-gradient' : ''}`}>вместе с нами!</h1>
+        </div>
+        <div className="sub-title">
+          <button 
+            onClick={handleJoinClick} // новый обработчик
+            className={`px-20 py-6 text-4xl font-bold text-rose-400 border border-rose-600 hover:bg-rose-600 hover:text-white focus:outline-none focus:ring active:bg-rose-500 ${!loading && subTitleAnimation ? 'animate-sub-title' : 'initial-hidden'}`}
+          >
+            Присоединиться
+          </button>
+        </div>
+      </div>
+      <div className="container-hero container-image-hero">
+        <Image src="/heroimg.svg" alt="Next Image" width={600} height={600} className={`svg-icon ${!loading && subTitleAnimation ? 'animate-svg-icon' : 'initial-offscreen-right'}`} />
+      </div>
+    </div>
   );
 }
