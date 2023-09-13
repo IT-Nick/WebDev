36c4fb2e7eb81d908a6db6e112d9c96dfebace0f
Stepan Plehanov <crayon-d@mail.ru>
2023-09-13 12:04:02 +0300

    check routes

--- services/general-management/backend/api/routes.go
+++ services/general-management/backend/api/routes.go
@@ -10,26 +10,27 @@ func NewRouter() *mux.Router {
 
 	// Оборачиваем все обработчики в CORS middleware
 	router.Use(utils.CorsMiddleware)
+	s := router.PathPrefix("/general-management").Subrouter()
 
 	// Маршруты API для пользователей
 	// создает новую заявку на вступление в команду или организацию.
-	router.HandleFunc("/api/applications/create", CreateApplicationHandler).Methods("POST")
+	s.HandleFunc("/api/applications/create", CreateApplicationHandler).Methods("POST")
 	// Авторизует пользователя в системе. (Новый)
-	router.HandleFunc("/api/users/auth", AuthUserHandler).Methods("POST")
+	s.HandleFunc("/api/users/auth", AuthUserHandler).Methods("POST")
 
 	// Маршруты API для администратора
 	// создает новое мероприятие.
-	router.HandleFunc("/api/events/create", CreateEventHandler).Methods("POST")
+	s.HandleFunc("/api/events/create", CreateEventHandler).Methods("POST")
 	// Удаляет существующее мероприятие.
-	router.HandleFunc("/api/events/delete", DeleteEventHandler).Methods("DELETE")
+	s.HandleFunc("/api/events/delete", DeleteEventHandler).Methods("DELETE")
 	// Одобряет заявку на вступление в сборную.
-	router.HandleFunc("/api/applications/approve", ApproveApplicationHandler).Methods("PUT")
+	s.HandleFunc("/api/applications/approve", ApproveApplicationHandler).Methods("PUT")
 	// Возвращает список всех заявок на вступление в сборную.
-	router.HandleFunc("/api/applications/list", ListApplicationsHandler).Methods("GET")
+	s.HandleFunc("/api/applications/list", ListApplicationsHandler).Methods("GET")
 	// Возвращает список всех мероприятий.
-	router.HandleFunc("/api/events/list", ListEventsHandler).Methods("GET")
+	s.HandleFunc("/api/events/list", ListEventsHandler).Methods("GET")
 	// Возвращает список всех одобренных пользователей из таблицы auth. (Новый)
-	router.HandleFunc("/api/users/list", ListAuthUsersHandler).Methods("GET")
+	s.HandleFunc("/api/users/list", ListAuthUsersHandler).Methods("GET")
 
 	return router
 }
