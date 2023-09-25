c1c081a7f4a0853de4c4d08e362e8f9fa2fff602
Stepan Plehanov <crayon-d@mail.ru>
2023-09-25 23:35:55 +0300

    as

--- frontends/mini-landings/components/shared/Navbar/Navbar.tsx
+++ frontends/mini-landings/components/shared/Navbar/Navbar.tsx
@@ -29,25 +29,35 @@ export default function Navbar() {
   }, []);
 
   return (
-      <nav
-        className="fixed top-0 left-0 right-0 z-50"
-      >
-        <div className="container py-2 flex items-center justify-between">
-          <div className={`${scrolling ? 'move-up-logo' : ''}`}>
+    <nav
+      className="fixed top-0 left-0 right-0 z-50 h-16 mt-4 ml-4 mr-4 pt-2"
+    >
+      <div className="container flex items-center justify-between">
+        <div className={`${scrolling ? 'move-up-logo flex-shrink-0 absolute top-0 left-0 mt-4' : 'flex-shrink-0 absolute top-0 left-0 mt-4'} d-flex align-items-center justify-content-center`} style={{ width: '60px', height: '60px' }}>
           <Link href="/">
-                <Image 
-                  src="/assets/PreLogo.svg" 
-                  alt="Logo" 
-                  width={150}
-                  height={150}
-                  className={logoClass}
-                />
-            </Link>
-          </div>
-          <div className={`${scrolling ? 'move-up-menu' : ''}`}>
-            <Menu />
-          </div>
+            {/* Белый круг на фоне */}
+            <svg width="60" height="60" style={{ position: 'absolute', zIndex: 1, transform: 'translateY(-10px)' }}>
+              <circle cx="30" cy="30" r="30" fill="white" />
+            </svg>
+
+            {/* Логотип */}
+            <Image
+              src="/assets/PreLogo.svg"
+              alt="Logo"
+              width={45}  // изменена ширина логотипа
+              height={45} // изменена высота логотипа
+              style={{ position: 'relative', zIndex: 2, transform: 'translateX(8px)' }}
+            />
+          </Link>
         </div>
-      </nav>
+
+
+
+
+        <div className={`${scrolling ? 'move-up-menu flex-shrink-0 absolute top-0 right-0' : 'flex-shrink-0 absolute top-0 right-0'}`}>
+          <Menu />
+        </div>
+      </div>
+    </nav>
   );
 }
