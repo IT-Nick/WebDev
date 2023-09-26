c0f306d7f8fef32bfed2ae5dc3b6bc32830d07e4
Stepan Plehanov <crayon-d@mail.ru>
2023-09-26 14:29:14 +0300

    as

--- frontends/mini-landings/components/admin/AddEventForm.tsx
+++ frontends/mini-landings/components/admin/AddEventForm.tsx
@@ -8,10 +8,11 @@ const AddEventForm: React.FC = () => {
         content: '',
         start_date: '',
         end_date: '',
-        image_url: ''
+        image_url: '',
+        registrationUrl: ''
     });
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setEventData(prevData => ({
             ...prevData,
@@ -20,11 +21,17 @@ const AddEventForm: React.FC = () => {
     };
 
     const convertToRFC3339 = (dateStr: string): string => {
-        return `${dateStr}T00:00:00Z`;
-    }
-    
+        const [day, month, year] = dateStr.split('.');
+        if (day && month && year) {
+            return `${year}-${month}-${day}T00:00:00Z`;
+        } else {
+            console.error("Неверный формат даты, нужно DD.MM.YYYY.");
+            return "";
+        }
+    };
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
         try {
             const dataToSend = {
                 ...eventData,
@@ -53,16 +60,16 @@ const AddEventForm: React.FC = () => {
     };
 
     return (
-        <div>
+        <div className="max-w-lg mx-auto">
             <h1 className="text-2xl font-bold mb-4">Добавить мероприятие</h1>
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     name="title"
                     placeholder="Название"
                     value={eventData.title}
                     onChange={handleInputChange}
-                    className="border p-2 mb-4 rounded-xl"
+                    className="block w-full border p-2 mb-4 rounded-xl"
                     maxLength={53}
                 />
                 <input
@@ -71,30 +78,32 @@ const AddEventForm: React.FC = () => {
                     placeholder="Краткое описание"
                     value={eventData.context}
                     onChange={handleInputChange}
-                    className="border p-2 mb-4 rounded-xl"
+                    className="block w-full border p-2 mb-4 rounded-xl"
                     maxLength={84}
                 />
-                <input
-                    type="text"
+                <textarea
                     name="content"
                     placeholder="Полное описание"
                     value={eventData.content}
                     onChange={handleInputChange}
-                    className="border p-2 mb-4 rounded-xl"
-                />
+                    className="block w-full border p-2 mb-4 rounded-xl"
+                    rows={3} // Высота в 3 строки по умолчанию
+                ></textarea>
                 <input
-                    type="date"
-                    name="Начало регистрации"
+                    type="text"
+                    name="start_date"
+                    placeholder="Начало регистрации (DD.MM.YYYY)"
                     value={eventData.start_date}
                     onChange={handleInputChange}
-                    className="border p-2 mb-4 rounded-xl"
+                    className="block w-full border p-2 mb-4 rounded-xl"
                 />
                 <input
-                    type="date"
-                    name="Конец регистрации"
+                    type="text"
+                    name="end_date"
+                    placeholder="Конец регистрации (DD.MM.YYYY)"
                     value={eventData.end_date}
                     onChange={handleInputChange}
-                    className="border p-2 mb-4 rounded-xl"
+                    className="block w-full border p-2 mb-4 rounded-xl"
                 />
                 <input
                     type="text"
@@ -102,13 +111,17 @@ const AddEventForm: React.FC = () => {
                     placeholder="Ссылка на изображение"
                     value={eventData.image_url}
                     onChange={handleInputChange}
-                    className="border p-2 mb-4 rounded-xl"
+                    className="block w-full border p-2 mb-4 rounded-xl"
                 />
-                <button
-                    type="submit"
-                    onClick={handleSubmit}
-                    className="bg-blue-500 text-white p-2 rounded-xl"
-                >
+                <input
+                    type="text"
+                    name="registrationUrl"
+                    placeholder="Ссылка для регистрации"
+                    value={eventData.registrationUrl}
+                    onChange={handleInputChange}
+                    className="block w-full border p-2 mb-4 rounded-xl"
+                />
+                <button type="submit" className="block w-full bg-blue-500 text-white p-2 rounded-xl">
                     Добавить
                 </button>
             </form>
