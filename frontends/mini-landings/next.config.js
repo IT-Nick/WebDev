4677e8b52d3071501e3992651915ef700e346374
Stepan Plehanov <crayon-d@mail.ru>
2023-09-26 18:18:36 +0300

    as

--- frontends/mini-landings/next.config.js
+++ frontends/mini-landings/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cruip-tutorials.vercel.app'],
-    },
-  }
-  
-  module.exports = nextConfig;
-  
+  images: {
+    domains: ['cruip-tutorials.vercel.app', 'team.mpei.ru'],
+  },
+}
+
+module.exports = nextConfig;
